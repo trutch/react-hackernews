e8a82048f547bdf6705f834ace68b077c286442d
Tyler Rutschman <tyler.rutschman@garmin.com>
2017-10-30 15:33:04 -0500

    Update app to fetch data from hackernews

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  { 
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  { 
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
+const DEFAULT_QUERY = 'redux';
 
-const isSearched = searchTerm => item => 
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
     
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
   }
 
+  fetchSearchTopStories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => e);
+  }
+
+  setSearchTopStories(result) {
+    this.setState( { result } );
+  }
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
+    const updatedHits = this.state.result.hits.filter(isNotId);
     console.log("Removing object: " + id);
-    this.setState({ list: updatedList });
+    this.setState({
+      result: { ...this.state.result, hits: updatedHits } 
+    });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    if (!result) { return null; }
     return (
       <div className="page">
         <div className="interactions">
@@ -60,14 +64,18 @@ class App extends Component {
           </Search>
         </div>
         <Table
-            list={list}
-            pattern={searchTerm}
-            onDismiss={this.onDismiss}
+          list={result.hits}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
         />
       </div>
     );
   }
 }
+
+const isSearched = searchTerm => item => 
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 
 const Search = ({ value, onChange, children }) =>
   <form>
